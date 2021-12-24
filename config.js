58bd7434f052cefadf3fde0402b9d99fe0981c14
HungBui-08 <96626339+HungBui-08@users.noreply.github.com>
2021-12-24 22:58:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introDesc: `Thanh xuân như 1 ly trà
+    Dù dậm dù nhật miễn là có Bầuu <3`,
+    btnIntro: 'ỏooooooooo',
+    title: 'Người ta cần người yêu đêm noel😙',
+    desc: 'Điều anh cần là 1 tình yêu no end ',
+    btnYes: 'Vẫn cứ là iu anh <33',
+    btnNo: 'eo ơi sến thế :3',
+    question:'Trên thế giới hơn 7 tỉ người biết vì sao anh yêu em không <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    reply: 'Vì em là điều tuyệt vời nhất đến với anh đấy <33333333',
+    mess: 'Anh biết mà ahihi 😘😘',
+    messDesc: 'Anh yêu em bằng sự chân thành và 1 cách trọn vẹn nhấttt
+    'Giáng Sinh vui vẻ nha baeee'
+    'btnAccept: ''Okiiiii lun <3',
+  
